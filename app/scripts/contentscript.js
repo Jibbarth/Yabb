b03000bcc90c9d68e70b011290e38c42c9782875
Jibé Barth <barth.jib@gmail.com>
2017-12-03 18:20:01 +0100

    Add highlight.js

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,4 +1,29 @@
 // Enable chromereload by uncommenting this line:
-// import 'chromereload/devonly'
+//import 'chromereload/devonly'
+const hljs = require('highlight.js');
+const $ = require("jquery")
+const waitRender = require('./wait-render.js');
 
-console.log(`'Allo 'Allo! Content script`)
+
+waitRender('.diff-container').then(() => {
+    console.info("[Yabb] Init Highlight");
+
+    $('pre.source').each(function(i, block) {
+        // Add language as class to help Highlight
+        var language = guessLanguage(block);
+        $(block).addClass(language);
+
+        hljs.highlightBlock(block);
+    });
+});
+
+/**
+ * Guess language of htmlElement by finding parents container with filename
+ * and retrieving extension
+ *
+ * @param {HTMLElement} block
+ */
+function guessLanguage(block)
+{
+    return $(block).parents('.iterable-item').data('identifier').split('.').pop();
+}
