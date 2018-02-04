ab9e20229e13c182a94c8b218ee7ab3d1983765b
Jibé Barth <barth.jib@gmail.com>
2018-02-04 16:21:26 +0100

    Reapply highlight even when change tabs

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,21 +1,71 @@
 // Enable chromereload by uncommenting this line:
 //import 'chromereload/devonly'
+import elementReady from 'element-ready';
+
 const hljs = require('highlight.js');
 const $ = require("jquery")
-const waitRender = require('./wait-render.js');
 
+init();
 
-waitRender('.diff-container').then(() => {
-    console.info("[Yabb] Init Highlight");
-
-    $('pre.source').each(function(i, block) {
-        // Add language as class to help Highlight
-        var language = guessLanguage(block);
-        $(block).addClass(language);
-
-        hljs.highlightBlock(block);
+function init()
+{
+    elementReady('.diff-container').then(() => {
+        highlight();
     });
-});
+}
+
+/**
+ * Check when dom change to reapply highlight
+ */
+function checkChange()
+{
+    elementReady('#pr-tab-content').then(() => {
+        // Configuration of the observer:
+        const config = { childList: true, subtree: true };
+        // Create an observer instance
+        const observer = new MutationObserver(function (mutations) {
+            var diff = document.querySelector('.diff-container');
+            if (null != diff) {
+                this.disconnect();
+                highlight();
+            }
+        });
+
+        const target = document.querySelector('#pr-tab-content');
+        observer.observe(target, config);
+    });
+}
+/**
+ * Apply highlight on dom
+*/
+function highlight()
+{
+    var diffContainer = document.querySelectorAll('.diff-container');
+
+    diffContainer.forEach(function(diff, index) {
+        var diffLineBlock = diff.querySelectorAll('.udiff-line');
+
+        diffLineBlock.forEach(function(block, i) {
+            cleanBlock(block);
+        });
+    });
+
+    checkChange();
+}
+/**
+ * Clean a block, guess language, init highlight
+ *
+ * @param {HTMLElement} block
+ */
+function cleanBlock(block){
+    var language = guessLanguage(block);
+    var sourceBlock = block.querySelector('pre.source');
+
+    if (null != sourceBlock) {
+        sourceBlock.classList.add(language);
+        hljs.highlightBlock(sourceBlock);
+    }
+}
 
 /**
  * Guess language of htmlElement by finding parents container with filename
